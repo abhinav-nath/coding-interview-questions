75685e83e8c476784517fb2fe503fae6e66a1949
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-11-25 09:47:24 +0530

    added 3 solutions

---
+++ src/com/codecafe/leetcode/easy/ClimbingStairs_70.java
@@ -0,0 +1,72 @@
+package com.codecafe.leetcode.easy;
+
+import java.util.Scanner;
+
+/*
+    Classic problem of Dynamic Programming
+    --------------------------------------
+    You are climbing a staircase. It takes n steps to reach the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    
+    Example 1:
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+
+    Example 2:
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+    
+    Constraints:
+    1 <= n <= 45
+
+ */
+
+public class ClimbingStairs_70 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int num = sc.nextInt();
+
+            if (num > 45 || num < 1) {
+                System.out.println("Invalid input");
+                break;
+            } else {
+                int numOfWays = climbStairs(num);
+                System.out.println("Number of ways to climb " + num + " stairs = " + numOfWays);
+            }
+        }
+
+        sc.close();
+    }
+
+    private static int climbStairs(int n) {
+
+        if (n == 1)
+            return 1;
+
+        if (n == 2)
+            return 2;
+
+        int[] ways = new int[n + 1];
+        ways[1] = 1;
+        ways[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            ways[i] = ways[i - 2] + ways[i - 1];
+        }
+
+        return ways[n];
+    }
+
+}
---
+++ src/com/codecafe/leetcode/easy/RemoveElement_27.java
@@ -0,0 +1,74 @@
+package com.codecafe.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+    Example 1:
+    Input: nums = [3,2,2,3], val = 3
+    Output: 2, nums = [2,2]
+    Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+    It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,3,0,0], your answer will be accepted.
+
+    Example 2:
+    Input: nums = [0,1,2,2,3,0,4,2], val = 2
+    Output: 5, nums = [0,1,4,0,3]
+    Explanation: Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+    Note that the order of those five elements can be arbitrary. It doesn't matter what values are set beyond the returned length.
+    
+    Constraints:
+    0 <= nums.length <= 100
+    0 <= nums[i] <= 50
+    0 <= val <= 100
+
+ */
+public class RemoveElement_27 {
+
+    public static void main(String[] args) {
+
+//        int nums[] = new int[] { 3, 2, 2, 3 };
+//        int val = 3;
+
+        int nums[] = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int val = 2;
+
+        System.out.println("\ninput array, length = " + nums.length);
+        for (int element : nums)
+            System.out.print(element + " ");
+
+        System.out.println();
+
+        int len = removeElement(nums, val);
+
+        System.out.println("\nafter removing duplicates, length = " + len);
+        for (int i = 0; i < len; i++)
+            System.out.print(nums[i] + " ");
+
+        System.out.println();
+    }
+
+    private static int removeElement(int[] nums, int val) {
+
+        if (nums.length == 0)
+            return 0;
+
+        // index to the front of array
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i]; // move non-matching element to the front of array
+                j++;
+            }
+        }
+
+        return j;
+    }
+
+}
