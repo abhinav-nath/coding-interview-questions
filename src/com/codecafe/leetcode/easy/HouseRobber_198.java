0bd37f78445cedc5ea7a83c17f09867c84e00c77
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-11-28 23:08:44 +0530

    added leetcode problem number 198 - HouseRobber

---
+++ src/com/codecafe/leetcode/easy/HouseRobber_198.java
@@ -0,0 +1,74 @@
+package com.codecafe.leetcode.easy;
+
+/*
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 400
+
+*/
+public class HouseRobber_198 {
+
+    public static void main(String[] args) {
+
+//        int nums[] = new int[] { 1, 2, 3, 1 };
+//        int nums[] = new int[] { 2, 7, 9, 3, 1 };
+//        int nums[] = new int[] { 2, 1, 1, 2 };
+        int nums[] = new int[] { 1, 2, 3, 1, 1, 100 };
+
+        System.out.println("Total amount you can rob = " + rob(nums));
+    }
+
+    // this approach does not work !!
+    private static int notSoGoodRob(int[] nums) {
+
+        int sumEven = 0, sumOdd = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0)
+                sumEven += nums[i];
+            else
+                sumOdd += nums[i];
+        }
+
+        if (sumEven > sumOdd)
+            return sumEven;
+        else
+            return sumOdd;
+    }
+
+    // uses dynamic programming and memoization
+    private static int rob(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        int dp[] = new int[nums.length + 1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+
+        return dp[nums.length];
+    }
+
+}
